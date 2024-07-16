c90ccb94a04beb6db16602003fea8ead27f76847
AdityaGupta1903 <guptaditya19@gmail.com>
2024-07-16 21:09:21 +0530

    Removed Unused Commits from main.tsx file

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { QueryClient, QueryClientProvider, useQuery } from 'react-query'
+import { QueryClient, QueryClientProvider } from 'react-query'
 const queryClient = new QueryClient()
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <QueryClientProvider client={queryClient} contextSharing={true}>
